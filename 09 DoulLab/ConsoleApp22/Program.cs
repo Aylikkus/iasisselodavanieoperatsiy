803a4a86a87cb648502c18a03d216893e2dc9c86
Игорь <66175573+Wyaro@users.noreply.github.com>
2024-01-21 21:44:54 +0300

    Update Program.cs
    change values

--- 09 DoulLab/ConsoleApp22/Program.cs
+++ 09 DoulLab/ConsoleApp22/Program.cs
@@ -1,4 +1,4 @@
-﻿using SimplexLib;
+using SimplexLib;
 using System;
 
 namespace ConsoleApp22
@@ -30,37 +30,60 @@ namespace ConsoleApp22
             le4.Sign = Sign.LessThanEqual;
             le4.B = 6;
             LinearExpression le5 = new LinearExpression(2);
-            le5[0] = 0;
+            le5[0] = 1;
             le5[1] = 0;
             le5.Sign = Sign.GreaterThanEqual;
             le5.B = 0;
+            LinearExpression le6 = new LinearExpression(2);
+            le6[0] = 0;
+            le6[1] = 1;
+            le6.Sign = Sign.GreaterThanEqual;
+            le6.B = 0;
 
-            Constraints constraints = new Constraints(5, 2);
+            Constraints constraints = new Constraints(6, 2);
             constraints[0] = le1;
             constraints[1] = le2;
             constraints[2] = le3;
             constraints[3] = le4;
             constraints[4] = le5;
+            constraints[5] = le6;
 
 
             SimplexMethod simplexMethod = new SimplexMethod(constraints, targetFunction);
             var res1 = simplexMethod.SolveWithSimplexMethod();
 
 
+            Constraints constraints2 = new Constraints(4, 2);
+            constraints2[0] = le1;
+            constraints2[1] = le2;
+            constraints2[2] = le3;
+            constraints2[3] = le4;
             //Работа с двойственной функцией
-            DataModel dataModel = new DataModel(targetFunction, le1, le2, le3, le4, le5);
-            dataModel.BuildingExtendedMatrix(dataModel.Constraints);
+            DataModel dataModel = new DataModel(targetFunction, constraints2);
+            dataModel.BuildingExtendedMatrix();
             dataModel.TransportationA();
             dataModel.GetDualFunction();
             dataModel.GetDualConstraints();
 
 
-            SimplexMethod simplexMethod1 = new SimplexMethod(dataModel.DualConstraints, dataModel.DualFunction);
-            var res2 = simplexMethod1.SolveWithSimplexMethod();
+            SimplexMethod simplexMethod2 = new SimplexMethod(dataModel.DualConstraints, dataModel.DualFunction);
+            var res2 = simplexMethod2.SolveWithSimplexMethod();
 
-            Console.WriteLine($"Целевая функция: {res1}");
-            Console.WriteLine($"Двойственная функция: {res2}");
+            Console.WriteLine($"Целевая функция:");
+            Print(res1);
+            Console.WriteLine($"Двойственная функция: ");
+            Print(res2);
+            Console.Read();
+        }
 
+        public static void Print((double[], double) res)
+        {
+            for (int i = 0; i < res.Item1.Length; i++)
+            {                
+                Console.Write($"X[{i}]={res.Item1[i]} ");
+            }
+            Console.Write($"F={res.Item2}");
+            Console.WriteLine();
         }
     }
 }
